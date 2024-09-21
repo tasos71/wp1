cf780dade579136b68e0057cd5ac51613955ee7a
Chalvatsiotis, Tasos <a.chalvatsiotis@accenture.com>
2024-09-21 20:43:49 +0200

    a lot

--- terraform/environments/us-region/main.tf
+++ terraform/environments/us-region/main.tf
@@ -6,11 +6,3 @@ module "us-network" {
   subnet1           = "10.4.1.0/24"
   subnet2           = "10.4.2.0/24"
 }
-
-#module "us-eu-peerirg" {
-#  source = "./modules/network-peerirg"
-#  var.name
-#  resource_group_name          = module.eu-conf.module.eu-network.azurerm_resource_group.rg
-#  virtual_network_name         = module.us-conf.module.us-network.azurerm_virtual_network.vnet
-#  remote_virtual_network_id    = var.remote_virtual_network_id
-#}
