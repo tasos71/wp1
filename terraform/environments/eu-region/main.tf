4a70b953d8478978816e5abdb000cae20388d589
Chalvatsiotis, Tasos <a.chalvatsiotis@accenture.com>
2024-09-21 20:23:05 +0200

    a lot

--- terraform/environments/eu-region/main.tf
+++ terraform/environments/eu-region/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+
+  features {}
+  subscription_id = "65008410-3554-490e-8d48-54b2b065d97a"
+}
+
 module "eu-network" {
   source            = "../../modules/network-conf"
   resource_group    = "test-tech_task-wp01--rg_eu_west"
