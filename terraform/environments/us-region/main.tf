a79b8f7f73d2f39d60362482b883e21df96122da
Chalvatsiotis, Tasos <a.chalvatsiotis@accenture.com>
2024-09-21 12:13:29 +0200

    fix

--- terraform/environments/us-region/main.tf
+++ terraform/environments/us-region/main.tf
@@ -1,5 +1,5 @@
 module "us-network" {
-  source            = "../modules/network-conf"
+  source            = "../../modules/network-conf"
   resource_group    = "tasos-tech_task-wp01--rg_us_west"
   region            = "westus"
 }
