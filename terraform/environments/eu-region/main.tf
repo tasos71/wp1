ee927b786d86333d534a5dacce003e851f3101f9
Chalvatsiotis, Tasos <a.chalvatsiotis@accenture.com>
2024-09-21 20:20:22 +0200

    a lot

--- terraform/environments/eu-region/main.tf
+++ terraform/environments/eu-region/main.tf
@@ -1,8 +1,19 @@
 module "eu-network" {
   source            = "../../modules/network-conf"
-  resource_group    = "tasos-tech_task-wp01--rg_eu_west"
+  resource_group    = "test-tech_task-wp01--rg_eu_west"
   region            = "westeurope"
   main_cidr         = "10.5.0.0/16"
   subnet1           = "10.5.1.0/24"
   subnet2           = "10.5.2.0/24"
+}
+
+resource "azurerm_network_security_group" "nsg-fileserver" {
+  name                = "${module.eu-network.rgname}-NSG-fileserver"
+  location            = "westeurope"
+  resource_group_name = module.eu-network.rgname
+}
+resource "azurerm_network_security_group" "nsg-admin" {
+  name                = "${module.eu-network.rgname}-NSG-Admin"
+  location            = "westeurope"
+  resource_group_name = module.eu-network.rgname
 }
--- terraform/environments/us-region/main.tf
+++ terraform/environments/us-region/main.tf
@@ -1,6 +1,6 @@
 module "us-network" {
   source            = "../../modules/network-conf"
-  resource_group    = "tasos-tech_task-wp01--rg_us_west"
+  resource_group    = "test-tech_task-wp01--rg_us_west"
   region            = "westus"
   main_cidr         = "10.4.0.0/16"
   subnet1           = "10.4.1.0/24"
