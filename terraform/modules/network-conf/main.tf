dc8b5693d3ad85441bfdce68d84af0c6c866ee25
Chalvatsiotis, Tasos <a.chalvatsiotis@accenture.com>
2024-09-21 11:50:10 +0200

    fix

--- terraform/modules/network-conf/main.tf
+++ terraform/modules/network-conf/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-  }
-}
-
-provider "azurerm" {
-
-  features {}
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group
   location = var.region
