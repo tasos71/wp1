2af2925cb3d3550ce7ede2acaad571119eae47df
Chalvatsiotis, Tasos <a.chalvatsiotis@accenture.com>
2024-09-21 14:04:23 +0200

    add peering fix

--- terraform/environments/eu-region/main.tf
+++ terraform/environments/eu-region/main.tf
@@ -9,8 +9,8 @@ module "eu-network" {
 
 module "eu-us-peerirg" {
   source                       = "../../modules/network-peerirg"
-  name                         = "${module.eu-network.resource_group}-to-us-peering"
-  resource_group_name          = "${module.us-network.resource_group}"
+  #name                         = "${module.eu-network.resource_group}-to-us-peering"
+  #resource_group_name          = "${module.us-network.resource_group}"
   #virtual_network_name         = "${module.eu-conf.module.eu-network.name}"
-  #remote_virtual_network_id    = "${module.us-conf.module.us-network.id}"
+  remote_virtual_network_id    = "${module.eu-conf.module.eu-network.azurerm_virtual_network.vnet}"
 }
